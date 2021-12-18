c4d4869db8f01536cdcc332534477ec75c500030
Pawel Laskarzewski <pawel.mat17@op.pl>
2021-12-18 23:58:17 +0100

    init

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,11 +1,2 @@
 import { ResponseMain } from "./../types/getTypes";
 import { app } from "../app";
-const port = 3000;
-
-app.get<"", ResponseMain, {}>("/", async (req, res) => {
-  res.send({ mesage: "Hello main get", additiona: 12 });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
